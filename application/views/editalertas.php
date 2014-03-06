24eb7de91389ef2889a80b1cd736d2768105efcd
marcelo <marcelo.cheruti@gmail.com>
2014-03-06 00:38:44 -0300

    Layout

--- application/views/editalertas.php
+++ application/views/editalertas.php
@@ -1,1 +1,169 @@
+<div class="container">
+	<form class="form-horizontal">
+		<fieldset>
 
+			<!-- Form Name -->
+			<legend>Edição do Alerta</legend>
+			<div class="controls controls-row">
+					<div class="form-group span26">
+						<label class="control-label" for="iPrecoDe">Preço : </label>
+						<div class="controls">
+							<select id="iPrecoDe" name="iPrecoDe" class="form-control">
+								<option value="1">Option one</option>
+								<option value="2">Option two</option>
+							</select>&nbsp;-&nbsp; <select id="iPrecoAte" name="iPrecoAte"
+								class="form-control">
+								<option value="1">Option one</option>
+								<option value="2">Option two</option>
+							</select>
+						</div>
+					</div>
+					<div class="form-group span26">
+						<label class="col-md-5 control-label" for="iMarca">Marca</label>
+						<div class="col-md-5">
+							<input id="iMarca" name="iMarca" placeholder="Marca"
+								class="form-control input-md" type="text">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row>
+				<div  class="form-group">
+				<label class="col-md-4 control-label" for="iAnoDe">Ano</label>
+				<div class="col-md-4">
+					<select id="iAnoDe" name="iAnoDe" class="form-control">
+						<option value="1">Option one</option>
+						<option value="2">Option two</option>
+					</select>&nbsp;-&nbsp; <select id="iAnoAte" name="iAnoAte"
+						class="form-control">
+						<option value="1">Option one</option>
+						<option value="2">Option two</option>
+					</select>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="iNumeroPortasDe">Número
+					de portas</label>
+				<div class="col-md-4">
+					<select id="iNumeroPortasDe" name="iNumeroPortasDe"
+						class="form-control">
+						<option value="1">Option one</option>
+						<option value="2">Option two</option>
+					</select>&nbsp;-&nbsp; <select id="iNumeroPortasAte"
+						name="iNumeroPortasAte" class="form-control">
+						<option value="1">Option one</option>
+						<option value="2">Option two</option>
+					</select>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="iQuilometragemDe">Quilometragem</label>
+				<div class="col-md-4">
+					<select id="iQuilometragemDe" name="iQuilometragemDe"
+						class="form-control">
+						<option value="1">Option one</option>
+						<option value="2">Option two</option>
+					</select>&nbsp;-&nbsp; <select id="iQuilometragemAte"
+						name="iQuilometragemAte" class="form-control">
+						<option value="1">Option one</option>
+				
+				</div>
+				<option value="2">Option two</option>
+				</select>
+			</div>
+
+
+
+			<div class="form-group">
+				<label class="col-md-5 control-label" for="iModelo">Modelo</label>
+				<div class="col-md-5">
+					<input id="iModelo" name="iModelo" placeholder="Modelo"
+						class="form-control input-md" type="text">
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="col-md-5 control-label" for="iCidade">Cidade</label>
+				<div class="col-md-5">
+					<input id="iCidade" name="iCidade" placeholder="Cidade"
+						class="form-control input-md" type="text">
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="col-md-5 control-label" for="iEstado">Estado</label>
+				<div class="col-md-5">
+					<input id="iEstado" name="iEstado" placeholder="Estado"
+						class="form-control input-md" type="text">
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="col-md-6 control-label" for="iFotos">Fotos</label>
+				<div class="col-md-6">
+					<select id="iFotos" name="iFotos" class="form-control">
+						<option value="1">Option one</option>
+						<option value="2">Option two</option>
+					</select>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="col-md-6 control-label" for="iTipoCombustivel">Tipo
+					Combustível</label>
+				<div class="col-md-6">
+					<select id="iTipoCombustivel" name="iTipoCombustivel"
+						class="form-control">
+						<option value="1">Option one</option>
+						<option value="2">Option two</option>
+					</select>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="col-md-6 control-label" for="iTransmissao">Transmissão</label>
+				<div class="col-md-6">
+					<select id="iTransmissao" name="iTransmissao" class="form-control">
+						<option value="1">Option one</option>
+						<option value="2">Option two</option>
+					</select>
+				</div>
+			</div>
+
+
+			<div class="form-group">
+				<label class="col-md-6 control-label" for="iTipo">Tipo</label>
+				<div class="col-md-6">
+					<select id="iTipo" name="iTipo" class="form-control">
+						<option value="1">Option one</option>
+						<option value="2">Option two</option>
+					</select>
+				</div>
+			</div>
+
+			<div class="control-group">
+				<div class="controls">
+					<label class="checkbox" for="checkboxes-0"> <input
+						name="checkboxes" id="checkboxes-0" value="true" type="checkbox">
+						Preço Resuzido
+					</label>
+				</div>
+			</div>
+
+</div>
+<div class="row">
+	<div class="form-group">
+		<label class="col-md-4 control-label" for="iSalvar"></label>
+		<div class="col-md-4">
+			<button id="iSalvar" name="iSalvar" class="btn btn-primary">Salvar</button>
+			<button id="iCancelar" name="iCancelar" class="btn btn-warning">Cancelar
+				Alerta</button>
+		</div>
+	</div>
+</div>
+
+</fieldset>
+</form>
+</div>

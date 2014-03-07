29f404a7b335a1478cd21284c85bc4e5cbc2097c
marcelocc <MarceloCC@hdi.com.br>
2014-03-07 07:58:54 -0300

    ajuste de dados

--- application/views/editalertas.php
+++ application/views/editalertas.php
@@ -16,12 +16,36 @@
 					<div class="form-group">
 						<div class="controls">
 							<select id="iPrecoDe" name="iPrecoDe" class="form-control span2">
-								<option value="1">Option one</option>
-								<option value="2">Option two</option>
+								<option value="0">Qualquer</option>
+								<option value="5000">5.000,00</option>
+								<option value="10000">10.000,00</option>
+								<option value="20000">20.000,00</option>
+								<option value="30000">30.000,00</option>
+								<option value="40000">40.000,00</option>
+								<option value="50000">50.000,00</option>
+								<option value="60000">60.000,00</option>
+								<option value="70000">70.000,00</option>
+								<option value="80000">80.000,00</option>
+								<option value="90000">90.000,00</option>
+								<option value="100000">100.000,00</option>
+								<option value="150000">150.000,00</option>
+								<option value="200000">200.000,00</option>
 							</select>&nbsp;-&nbsp; <select id="iPrecoAte" name="iPrecoAte"
 								class="form-control span2">
-								<option value="1">Option one</option>
-								<option value="2">Option two</option>
+								<option value="0">Qualquer</option>
+								<option value="5000">5.000,00</option>
+								<option value="10000">10.000,00</option>
+								<option value="20000">20.000,00</option>
+								<option value="30000">30.000,00</option>
+								<option value="40000">40.000,00</option>
+								<option value="50000">50.000,00</option>
+								<option value="60000">60.000,00</option>
+								<option value="70000">70.000,00</option>
+								<option value="80000">80.000,00</option>
+								<option value="90000">90.000,00</option>
+								<option value="100000">100.000,00</option>
+								<option value="150000">150.000,00</option>
+								<option value="200000">200.000,00</option>
 							</select>
 						</div>
 					</div>
@@ -32,8 +56,8 @@
 				</div>
 				<div class="span4">
 					<select id="iFotos" name="iFotos" class="form-control">
-						<option value="1">Option one</option>
-						<option value="2">Option two</option>
+						<option value="0">Indiferente</option>
+						<option value="2">Só anúncios com imagens</option>
 					</select>
 				</div>
 			</div>
@@ -64,8 +88,13 @@
 				<div class="span4">
 					<select id="iTipoCombustivel" name="iTipoCombustivel"
 						class="form-control">
-						<option value="1">Option one</option>
-						<option value="2">Option two</option>
+						<option value="0">Indiferente</option>
+						<option value="1">Gasolina</option>
+						<option value="2">Flex</option>
+						<option value="3">Diesel</option>
+						<option value="4">Álcool</option>
+						<option value="5">GNV</option>
+						<option value="6">Elétrico</option>
 					</select>
 				</div>
 			</div>
@@ -80,12 +109,18 @@
 						<div class="form-controls">
 							<select id="iNumeroPortasDe" name="iNumeroPortasDe"
 								class="form-control span2">
-								<option value="1">Option one</option>
-								<option value="2">Option two</option>
+								<option value="0">Qualquer</option>
+								<option value="2">2 portas</option>
+								<option value="3">3 portas</option>
+								<option value="4">4 portas</option>
+								<option value="5">5 portas</option>
 							</select>&nbsp;-&nbsp; <select id="iNumeroPortasAte"
 								name="iNumeroPortasAte" class="form-control span2">
-								<option value="1">Option one</option>
-								<option value="2">Option two</option>
+								<option value="0">Qualquer</option>
+								<option value="2">2 portas</option>
+								<option value="3">3 portas</option>
+								<option value="4">4 portas</option>
+								<option value="5">5 portas</option>
 							</select>
 						</div>
 					</div>
@@ -96,8 +131,9 @@
 				</div>
 				<div class="span4">
 					<select id="iTransmissao" name="iTransmissao" class="form-control">
-						<option value="1">Option one</option>
-						<option value="2">Option two</option>
+						<option value="0">Indiferente</option>
+						<option value="1">Manual</option>
+						<option value="2">Automático</option>
 					</select>
 				</div>
 			</div>
@@ -112,12 +148,36 @@
 						<div class="form-controls">
 							<select id="iQuilometragemDe" name="iQuilometragemDe"
 								class="form-control span2">
-								<option value="1">Option one</option>
-								<option value="2">Option two</option>
+								<option value="0">Qualquer</option>
+								<option value="5000">5.000 km</option>
+								<option value="10000">10.000 km</option>
+								<option value="20000">20.000 km</option>
+								<option value="30000">30.000 km</option>
+								<option value="40000">40.000 km</option>
+								<option value="50000">50.000 km</option>
+								<option value="60000">60.000 km</option>
+								<option value="70000">70.000 km</option>
+								<option value="80000">80.000 km</option>
+								<option value="90000">90.000 km</option>
+								<option value="100000">100.000 km</option>
+								<option value="150000">150.000 km</option>
+								<option value="200000">200.000 km</option>
 							</select>&nbsp;-&nbsp; <select id="iQuilometragemAte"
 								name="iQuilometragemAte" class="form-control span2">
-								<option value="1">Option one</option>
-								<option value="2">Option two</option>
+								<option value="0">Qualquer</option>
+								<option value="5000">5.000 km</option>
+								<option value="10000">10.000 km</option>
+								<option value="20000">20.000 km</option>
+								<option value="30000">30.000 km</option>
+								<option value="40000">40.000 km</option>
+								<option value="50000">50.000 km</option>
+								<option value="60000">60.000 km</option>
+								<option value="70000">70.000 km</option>
+								<option value="80000">80.000 km</option>
+								<option value="90000">90.000 km</option>
+								<option value="100000">100.000 km</option>
+								<option value="150000">150.000 km</option>
+								<option value="200000">200.000 km</option>
 							</select>
 						</div>
 					</div>
@@ -128,8 +188,16 @@
 				</div>
 				<div class="span4">
 					<select id="iTipo" name="iTipo" class="form-control">
-						<option value="1">Option one</option>
-						<option value="2">Option two</option>
+						<option value="0">Indifetente</option>
+						<option value="1">Hatch</option>
+						<option value="2">Sedã</option>
+						<option value="3">Utilitário Esportivo</option>
+						<option value="4">Van</option>
+						<option value="5">Picape</option>
+						<option value="6">Conversível</option>
+						<option value="7">Jipe</option>
+						<option value="8">Blindados</option>
+						<option value="9">Utilitário</option>
 					</select>
 				</div>
 			</div>

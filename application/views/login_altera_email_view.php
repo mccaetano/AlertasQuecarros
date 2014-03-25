fb33cd62d01803aea4b43ed5c904bf5421777941
marcelocc <MarceloCC@hdi.com.br>
2014-03-25 00:37:31 -0300

    dev

---
+++ application/views/login_altera_email_view.php
@@ -0,0 +1,32 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?> 
+<div class="container well well-small">
+	<div class="row">
+		<div class="span12">
+			<?php echo form_open("usuario/alteraemail", array("class"=>"form-horizontal")) . PHP_EOL ?>
+					<fieldset>
+
+						<!-- Form Name -->
+						<legend>Altera o email do usuário logado</legend>
+
+						<!-- Appended Input-->
+						<div class="control-group">
+							<div class="controls">									
+								<input id="iOldEmail" name="iOldEmail" class="input-xlarge"
+									placeholder="preencha aqui seu novo endereço de email" type="text"
+									disabled value='<?php echo $email; ?>' >
+								<input id="iNewEmail" name="iNewEmail" class="input-xlarge"
+									placeholder="preencha aqui seu novo endereço de email" type="text"
+									required="" value='<?php set_value('iNewEmail') ?>' >
+							</div>
+						</div>
+						<div class="control-group">
+							<div class="controls">
+								<button type="submit" class="btn">Gravar</button>
+							</div>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>

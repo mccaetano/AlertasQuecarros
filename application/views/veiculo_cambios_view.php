f9c843152c3137674cd0660e6eb9ae6f88be6cf6
marcelocc <MarceloCC@hdi.com.br>
2014-03-31 18:24:25 -0300

    de

--- application/views/veiculo_cambios_view.php
+++ application/views/veiculo_cambios_view.php
@@ -2,6 +2,6 @@
 echo "							<option value=\"0\" " . ($selectedvalue == 0 ? "selected" : "") .  ">Indiferente</option>" . PHP_EOL;
 if ($cambios) {
 	foreach ($cambios as $cambio) {
-		echo "							<option value=\"" . $cambio->cd_cambio . "\" " . ($selectedvalue == $cambio->cd_cambio ? "selected" : "") .  ">" . $cambio->st_cambio . "</option>" . PHP_EOL;
+		echo "							<option value=\"" . $cambio->cd_cambio . "\" " . ($selectedvalue == $cambio->cd_cambio ? "selected" : "") .  ">" . mb_convert_encoding($cambio->st_cambio, 'iso-8859-1') . "</option>" . PHP_EOL;
 	}
 }
